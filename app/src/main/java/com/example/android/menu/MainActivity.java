f4065b20779831ef465a2efbeed9a6218517e8c8
Richard Huynh <huynhtastic@gmail.com>
2016-07-16 17:15:47 -0500

    initial commit; application completed

---
+++ app/src/main/java/com/example/android/menu/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.android.menu;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void printToLogs(View view) {
+        // Find first menu item TextView and print the text to the logs
+        TextView mango = (TextView) findViewById(R.id.menu_item_1);
+        String menuItem1 = mango.getText().toString();
+        Log.v("MainActivity", menuItem1);
+
+        // Find second menu item TextView and print the text to the logs
+        TextView blueberry = (TextView) findViewById(R.id.menu_item_2);
+        String menuItem2 = blueberry.getText().toString();
+        Log.v("MainActivity", menuItem2);
+
+        // Find third menu item TextView and print the text to the logs
+        TextView chocolate = (TextView) findViewById(R.id.menu_item_3);
+        String menuItem3 = chocolate.getText().toString();
+        Log.v("MainActivity", menuItem3);
+
+    }
+}
